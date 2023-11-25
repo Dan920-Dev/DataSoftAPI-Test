57c66d2732ecdf0b9631eae002a2d375b6ca472d
Dan920-Dev <00138218@uca.edu.sv>
2023-11-25 14:25:25 -0600

    Create Project

---
+++ src/main/java/com/example/datasofttestapi/DataSoftTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.datasofttestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSoftTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSoftTestApiApplication.class, args);
+    }
+
+}
